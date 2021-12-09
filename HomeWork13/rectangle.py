66c69c9ce0b7cb9940f4f294e85a7c7005ebe3be
Gaposhaa <95427268+Gaposhaa@users.noreply.github.com>
2021-12-09 23:51:09 +0300

    Add files via upload

---
+++ HomeWork13/rectangle.py
@@ -0,0 +1,36 @@
+class Rectangle:
+    def __init__(self, x, y, high, width):
+        self.x = x
+        self.y = y
+        self.high = high
+        self.width = width
+        self.Point_first = x, y
+        self.Point_second = x + width, y
+        self.Point_third = x, y + high
+        self.Point_fourth = x + width, y + high
+    def move(self, distance, direction):
+        if direction == "left":
+            self.Point_first = self.x -distance
+            self.Point_second = self.x - distance
+            self.Point_third = self.x - distance
+            self.Point_fourth = (self.x + self.width) - distance
+        elif direction == "right":
+            self.Point_first = self.x + distance
+            self.Point_second = self.x + distance
+            self.Point_third = self.x + distance
+            self.Point_fourth = (self.x + self.width) + distance
+        elif direction == "up":
+            self.Point_first = self.y + distance
+            self.Point_second = self.y + distance
+            self.Point_third = self.y + distance
+            self.Point_fourth = (self.y + self.high) + distance
+        elif direction == "down":
+            self.Point_first = self.y - distance
+            self.Point_second = self.y - distance
+            self.Point_third = self.y - distance
+            self.Point_fourth = (self.y + self.high) - distance
+    def current_coord(self):
+        return (self.Point_first, self.Point_second, self.Point_third, self.Point_fourth)
+
+
+
